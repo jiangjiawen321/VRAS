3eb0110448f185bc28ab15eeceaffeb6a1650699
Jiahang Li <115770276+rekkles2@users.noreply.github.com>
2025-04-07 23:20:02 +0800

    Update VRAS-Conv4.py

--- models/backbones/VRAS-Conv4.py
+++ models/backbones/VRAS-Conv4.py
@@ -34,52 +34,6 @@ class EMA(nn.Module):
         return (group_x * weights.sigmoid()).reshape(b, c, h, w)
 
 
-# class BackBone_new(nn.Module):
-#
-#     def __init__(self,num_channel=64):
-#         super().__init__()
-#         ## 编码器
-#         self.c1 = ConvBlock(3,num_channel)
-#         self.ru1 = nn.ReLU(inplace=True)
-#         self.pool = nn.MaxPool2d(2)
-#         self.c2 = ConvBlock(num_channel,num_channel)
-#         # nn.ReLU(inplace=True)
-#         # nn.MaxPool2d(2)
-#         self.c3 = ConvBlock(num_channel,num_channel)
-#         # nn.ReLU(inplace=True)
-#         # nn.MaxPool2d(2)
-#         self.c4 = ConvBlock(num_channel,num_channel)
-#         # nn.ReLU(inplace=True)
-#         # nn.MaxPool2d(2)
-#
-#         ## 解码器
-#         self.c5 = ConvBlock(128,num_channel)
-#         self.ru2 = nn.ReLU(inplace=True)
-#         self.c6 = ConvBlock(128, num_channel)
-#         self.c7 = ConvBlock(128, num_channel)
-#
-#         self.tmp_conv = nn.conv = nn.Conv2d(in_channels=64, out_channels=64, kernel_size=8, stride=8)
-#
-#     def forward(self,inp):
-#         # return self.layers(inp)
-#         ## 编码器
-#         R1 = self.pool(self.ru1(self.c1(inp)))
-#         R2 = self.pool(self.ru1(self.c2(R1)))
-#         R3 = self.pool(self.ru1(self.c3(R2)))
-#         R4 = self.pool(self.ru1(self.c4(R3)))
-#
-#         ## 解码器
-#         o1 = self.ru2(self.c5(torch.cat((R3, self.upsample_1(R4)), dim=1)))
-#         o2 = self.ru2(self.c6(torch.cat((R2, self.upsample_2(o1)), dim=1)))
-#         o3 = self.ru2(self.c7(torch.cat((R1, self.upsample_1(o2)), dim=1)))
-#
-#         return self.tmp_conv(o3)
-#
-#     def upsample_1(self, feature_map):
-#         return nn.functional.interpolate(feature_map, scale_factor=2, mode='bilinear', align_corners=False)
-#
-#     def upsample_2(self, feature_map):
-#         return F.interpolate(feature_map, size=(21, 21), mode='bilinear', align_corners=False)
 
 class BackBone_final(nn.Module):
 
